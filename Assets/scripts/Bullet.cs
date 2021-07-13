533dc5000aebe1db783d7e5b3ac4b6f547da6961
jake gergen <jake.gergen714@gmail.com>
2021-07-13 16:21:04 -0700

    Started work on spawning bullets

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -1,13 +1,17 @@
-﻿namespace DefaultNamespace
+﻿using UnityEngine;
+
+namespace DefaultNamespace
 {
     /**
      * Bullet with caliber and muzzle velocity
      */
-    public class Bullet
+    public class Bullet : MonoBehaviour
     {
         private readonly Caliber caliber;
         private readonly float muzzleVelocity;
-
+        private Rigidbody rigidbody;
+        private SphereCollider collider;
+        private bool hasFired = false;
         public Bullet(Caliber caliber, float muzzleVelocity)
         {
             this.caliber = caliber;
@@ -16,7 +20,12 @@
 
         public void fireBullet()
         {
-            
+            if (!hasFired)
+            {
+                hasFired = true;
+                this.rigidbody.AddRelativeForce(transform.forward * muzzleVelocity);
+                GameObject.Instantiate(this);
+            }
         }
 
     }
