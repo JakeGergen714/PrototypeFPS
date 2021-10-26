43c067ee0646d953b2b6c560b21a7c6d474134b0
jake gergen <jake.gergen714@gmail.com>
2021-10-25 18:18:23 -0700

    added null check for person hit box collision when searching for things that ouch. in case we collide wit somethin that dont ouch

--- Assets/scripts/player/component/PersonHitBox.cs
+++ Assets/scripts/player/component/PersonHitBox.cs
@@ -8,16 +8,21 @@ namespace player
     public class PersonHitBox : MonoBehaviour
     {
         [SerializeField] private float damageMultipler;
-        [SerializeField] private CapsuleCollider collider;
+        [SerializeField] private Collider collider;
 
         private void Awake()
         {
-            collider = GetComponent<CapsuleCollider>();
+            collider = GetComponent<Collider>();
         }
 
         private void OnCollisionEnter(Collision other)
         {
             IPersonOuchie thingThatOuches = other.gameObject.GetComponent<IPersonOuchie>();
+            if (Object.ReferenceEquals(null, thingThatOuches))
+            {
+                Debug.Log("null");
+                return;
+            }
             Debug.Log("I got ouched by this much: " + thingThatOuches.getAmountOfOuch() + " then multiplied it by this much: " +
                       damageMultipler + " to get: " + thingThatOuches.getAmountOfOuch() * damageMultipler);
         }
